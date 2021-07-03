594eec0a6a4db4ef1f1bf5ed25a50588e8d9d606
Welisson <welissonluca17@gmail.com>
2021-07-03 20:28:18 -0300

    feat: add players service

---
+++ src/players/players.service.ts
@@ -0,0 +1,29 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { CreatePlayerDTO } from './dtos/CreatePlayer.dto';
+import { Player } from './interfaces/Player.interface';
+import { v4 as uuid } from 'uuid';
+@Injectable()
+export class PlayersService {
+  private players: Player[] = [];
+  private readonly logger = new Logger(PlayersService.name);
+  async createPlayer(createPlayerDTO: CreatePlayerDTO): Promise<void> {
+    this.logger.log(`createPlayerDTO: ${createPlayerDTO}`);
+    this.create(createPlayerDTO);
+  }
+
+  private create(createPlayerDTO: CreatePlayerDTO): void {
+    const { name, phone, email } = createPlayerDTO;
+
+    const player: Player = {
+      id: uuid(),
+      name,
+      phone,
+      email,
+      ranking: 'A',
+      positionRanking: 1,
+      urlPlayerPhoto: 'www.teste/123.jpg',
+    };
+
+    this.players.push(player);
+  }
+}
