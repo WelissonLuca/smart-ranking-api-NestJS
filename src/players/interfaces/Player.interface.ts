675661f840ccb7818f42702761c6a55b43437d05
Welisson <welissonluca17@gmail.com>
2021-07-03 00:30:17 -0300

    feat: add interface player

---
+++ src/players/interfaces/Player.interface.ts
@@ -0,0 +1,9 @@
+export interface Player {
+  readonly id: string;
+  readonly phone: number;
+  readonly email: string;
+  name: string;
+  ranking: string;
+  positionRanking: number;
+  urlPlayerPhoto: string;
+}
