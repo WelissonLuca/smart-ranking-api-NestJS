1ae3677e20bbecb1010e2b1fca49f7261da36164
Welisson <welissonluca17@gmail.com>
2021-07-04 11:44:43 -0300

    feat: add schema players

---
+++ src/players/interfaces/Player.schema.ts
@@ -0,0 +1,19 @@
+import * as mongoose from 'mongoose';
+
+export const PlayerSchema = new mongoose.Schema(
+  {
+    phone: {
+      type: String,
+      unique: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+    },
+    name: String,
+    ranking: String,
+    positionRanking: String,
+    urlPlayerPhoto: String,
+  },
+  { timestamps: true, collection: 'players' },
+);
