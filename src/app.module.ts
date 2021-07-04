ae3f9d4c7d4cd15d4773949acab7f17cc69f1c04
Welisson <welissonluca17@gmail.com>
2021-07-04 09:56:01 -0300

    feat: add mongoose connection module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PlayersModule } from './players/players.module';
 
 @Module({
-  imports: [PlayersModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://welisson:123@cluster0.vrtmz.mongodb.net/smart_banking?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: true,
+      },
+    ),
+    PlayersModule,
+  ],
   controllers: [],
   providers: [],
 })
